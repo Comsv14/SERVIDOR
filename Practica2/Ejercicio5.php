3882d1946e7884c03525f9018bd8f215f544cc47
Lucas Seiz Valdes <czd22983@educastur.es>
2024-10-07 14:24:12 +0200

    ultima

--- Practica2/Ejercicio5.php
+++ Practica2/Ejercicio5.php
@@ -3,5 +3,17 @@
 a. Imprimir todos los elementos en forma sucesiva tomándolos por fila.
 b. Igual al anterior pero por columna.
 */
+$num = array();
+for ($i=0; $i < 3; $i++) { 
+    for ($j=0; $j < 5; $j++) { 
+        $num[$i][$j] = rand(0, 10);
+    }
+}
+
+foreach ($num as $fila) {
+    echo $num[][];
+    echo "<br>";
+}
+
 
 ?>
