ef4d50c2ca61cfa7b450c7ac35bfb218c3aeb2bc
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-11-14 12:49:43 +0100

    comit

--- Autoevaluacion/ejercicio1.php
+++ Autoevaluacion/ejercicio1.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio1</title>
+    <title>Convertir binario</title>
 </head>
 <body>
+    <h2>Convierte a binario</h2>
+    <form method="post">
 <?php
 for ($i=0; $i < 2; $i++) { 
     for ($j=0; $j < 3 ; $j++) { 
@@ -14,6 +16,26 @@ for ($i=0; $i < 2; $i++) {
     echo "<br>";
 }
 ?>
-<buttom type
+<buttom type="submit" name="calcular">Calcular</buttom>
+</form>
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST['calcular'])){
+    $numeros = $_POST['numero'];
+    echo "<h3>Resultado en Binario </h3>";
+    echo "<table>";
+    foreach($numeros as $fila){
+        echo "<tr>";
+        foreach($fila as $numeros){
+            if ($numero >=1 && $numero <=100 ){
+                echo "</td>" . decbin($numero) . "</td>";
+            }else {
+                echo "<td>Invalido</td>";
+            }
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
 </body>
 </html>
